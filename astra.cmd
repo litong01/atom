b7ed410040d7b77ad084c86356486641c5606d98
Tong Li <tli@netapp.com>
2023-02-10 16:54:31 -0500

    Added windows executable
    Signed-off-by: Tong Li <tli@netapp.com>

---
+++ astra.cmd
@@ -0,0 +1,17 @@
+@echo off
+docker run --rm --name astra --network host ^
+   -e "SKIP_KUBE_CONTEXT_CHECK=true" ^
+   -e "DEPLOY_TYPE=dev" ^
+   -e "VAULT_ADDR=%VAULT_ADDR%" ^
+   -e "GITHUB_USERNAME=%GITHUB_USERNAME%" ^
+   -e "GITHUB_TOKEN=%GITHUB_TOKEN%" ^
+   -e "GITHUB_ID=%GITHUB_ID%" ^
+   -e "CREDS_AUTH_CLIENT_ID=%CREDS_AUTH_CLIENT_ID%" ^
+   -e "CREDS_AUTH_DOMAIN=%CREDS_AUTH_DOMAIN%" ^
+   -e "CREDS_ISSUER_URL=%CREDS_ISSUER_URL%" ^
+   -e "CREDS_IMAGE_USERNAME=%CREDS_IMAGE_USERNAME%" ^
+   -e "CREDS_IMAGE_PASSWORD=%CREDS_IMAGE_PASSWORD%" ^
+   -v /var/run/docker.sock:/var/run/docker.sock ^
+   -v /tmp/astra:/tmp/astra ^
+   -v %CD%:/home/polaris ^
+   -v %USERPROFILE%/.kube:/home/.kube tli551/astra astra %*
