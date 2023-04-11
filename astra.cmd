20594e9c13838108733e74e5d99bbe9fb9ad2d02
Tong Li <tli@netapp.com>
2023-04-11 16:07:16 -0400

    Removed unused variables
    Signed-off-by: Tong Li <tli@netapp.com>

--- astra.cmd
+++ astra.cmd
@@ -17,7 +17,7 @@ docker run -it --rm --name astra --network host ^
    -e "CREDS_ISSUER_URL=%CREDS_ISSUER_URL%" ^
    -e "CREDS_IMAGE_USERNAME=%CREDS_IMAGE_USERNAME%" ^
    -e "CREDS_IMAGE_PASSWORD=%CREDS_IMAGE_PASSWORD%" ^
-   -v /var/run/docker.sock:/var/run/docker.sock %_pcloud2mount% ^
+   -v /var/run/docker.sock:/var/run/docker.sock ^
    -v %TEMP%/astra:/home/work/astra ^
    -v %CD%:/home/polaris -v %USERPROFILE%/.kube:/home/.kube ^
    ghcr.io/netapp-polaris/polaris/astra/astradt:v0.1.0 time astra %*
