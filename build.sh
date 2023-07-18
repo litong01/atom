e3d352d043e223af5e808f6149b288ca73e76e38
Tong Li <tli@netapp.com>
2023-07-18 12:24:39 -0400

    Fixed image path for posting images
    Signed-off-by: Tong Li <tli@netapp.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # build and remove dangling images in the system
-docker build -t ghcr.io/netapp-polaris/neptune/astra/astra3dt:v0.1.0 .
+docker build -t ghcr.io/litong01/astra/astra3dt:v0.1.0 .
 docker rmi -f $(docker images -f "dangling=true" -q)
