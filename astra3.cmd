3f084b225701f63d9fd97d167a9f78e14e3ee1ae
Tong Li <tli@netapp.com>
2023-07-31 15:45:18 -0400

    updated the windows script
    Signed-off-by: Tong Li <tli@netapp.com>

--- astra3.cmd
+++ astra3.cmd
@@ -1,15 +1,5 @@
 @echo off
 docker run -it --rm --name astra3 --network host ^
-   -e "SKIP_KUBE_CONTEXT_CHECK=true" ^
-   -e "DEPLOY_TYPE=dev" ^
-   -e "WORKDIR=/home/work/astra" ^
-   -e "HOSTWORKDIR=%TEMP%/astra" ^
-   -e "VAULT_ADDR=%VAULT_ADDR%" ^
-   -e "GITHUB_USERNAME=%GITHUB_ID%" ^
-   -e "GITHUB_TOKEN=%GITHUB_TOKEN%" ^
-   -e "GITHUB_ID=%GITHUB_ID%" ^
-   -e "DH_ID=%DH_ID%" ^
-   -e "DH_TOKEN=%DH_TOKEN%" ^
    -v /var/run/docker.sock:/var/run/docker.sock ^
    -v %TEMP%/astra3:/home/work/astra3 ^
    -v %CD%:/home/neptune -v %USERPROFILE%/.kube:/home/.kube ^
