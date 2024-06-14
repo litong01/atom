b61afd3e5e3f73cc1d724bb5b26d359eb401f2af
Tong Li <tli@netapp.com>
2024-06-14 14:45:30 -0400

    first commit
    Signed-off-by: Tong Li <tli@netapp.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # build and remove dangling images in the system
-docker build -t tli551/astra3dt:v0.1.0 .
+docker build -t tli551/atom:v0.1.0 .
 docker rmi -f $(docker images -f "dangling=true" -q)
